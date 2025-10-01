f92fdb07ed79339af4474a4d58fa9d80676fb7dd
louis.guichard-montguers <louis.guichard-montguers@etu.uca.fr>
2025-10-01 19:27:11 +0200

    add menu

--- myViedoJet/src/app/app.routes.ts
+++ myViedoJet/src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { MovieFormComponent } from './components/movie-form/movie-form.component';
 import { MovieHomeComponent } from './components/movie-home/movie-home.component';
 import { LoginComponent } from './components/login/login.component';
+import { MovieMenuComponent } from './components/movie-menu/movie-menu.component';
 
 export const routes: Routes = [
     {
         path: 'movie/form', component: MovieFormComponent
+    },
+    {
+        path: 'movie/menu', component: MovieMenuComponent
     },
     {
         path: 'movie/home', component: MovieHomeComponent
