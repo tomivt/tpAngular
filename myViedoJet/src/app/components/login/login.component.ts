65909d152a41e3e52baace456fde9a77ab79fe38
louis.guichard-montguers <louis.guichard-montguers@etu.uca.fr>
2025-10-01 18:08:41 +0200

    login page

--- myViedoJet/src/app/components/login/login.component.ts
+++ myViedoJet/src/app/components/login/login.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  loginForm: FormGroup = new FormGroup({
+    usernameForm: new FormControl('', Validators.required),
+    passwordForm: new FormControl('', Validators.required)
+  })
+
+  
 }
