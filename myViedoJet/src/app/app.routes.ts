830d871979722d81ad00d55d9c80df19aad43a7d
tomivt <nguyentommy0708@icloud.com>
2025-10-01 18:15:08 +0200

    add router

--- myViedoJet/src/app/app.routes.ts
+++ myViedoJet/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { MovieFormComponent } from './components/movie-form/movie-form.component';
+import { MovieHomeComponent } from './components/movie-home/movie-home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'movie/form', component: MovieFormComponent
+    },
+    {
+        path: 'movie/home', component: MovieHomeComponent
+    }
+];
