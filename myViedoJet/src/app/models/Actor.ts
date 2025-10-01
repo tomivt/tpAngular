6a70ee98ef8e37517490c58bf8969f08eb6b9ae1
tomivt <nguyentommy0708@icloud.com>
2025-10-01 17:48:58 +0200

    complete models

--- myViedoJet/src/app/models/Actor.ts
+++ myViedoJet/src/app/models/Actor.ts
@@ -1,3 +1,4 @@
 export interface Actor {
-    
+    id: number,
+    name: string
 }
--- myViedoJet/src/app/models/Movie.ts
+++ myViedoJet/src/app/models/Movie.ts
@@ -1,3 +1,9 @@
+import { Actor } from "./Actor";
+import { Category } from "./Category";
+
 export interface Movie {
-    
+    id: number,
+    title: string,
+    category: Category,
+    actors: Actor[]
 }
