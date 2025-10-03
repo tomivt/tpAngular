a9c671b033dd82b2d94b98dfa2471ff46fb89a76
louis.guichard-montguers <louis.guichard-montguers@etu.uca.fr>
2025-10-03 08:54:24 +0200

    ajout bouton vu

--- myViedoJet/src/app/components/movie-home/movie-home.component.ts
+++ myViedoJet/src/app/components/movie-home/movie-home.component.ts
@@ -3,23 +3,41 @@ import {VideoService} from '../../services/video.service';
 import {Movie} from '../../models/Movie';
 import {RouterLink} from '@angular/router';
 import { MovieMenuComponent } from '../movie-menu/movie-menu.component';
+import { CommonModule } from '@angular/common';
+
+type MovieWithSeen = Movie & { seen: boolean };
 
 @Component({
   selector: 'app-movie-home',
   imports: [
-    RouterLink, MovieMenuComponent
+    RouterLink, MovieMenuComponent, CommonModule,
   ],
   templateUrl: './movie-home.component.html',
   styleUrl: './movie-home.component.css'
 })
+
+
+
 export class MovieHomeComponent {
-  protected movies: Movie[] = []
+  protected movies: MovieWithSeen[] = []
   constructor(private service: VideoService) { }
 
   ngOnInit() {
+
     this.service.getMovies().subscribe(data => {
-        this.movies = data
-        console.log(data)
-      });
+    this.movies = data.map(movie => ({ ...movie, seen: false }));
+    console.log(data)
+  });
+
+  
+      
   }
+
+  toggleSeen(movie: MovieWithSeen) {
+    movie.seen = !movie.seen;
+    console.log(movie.name, 'vu =', movie.seen);
+  }
+
+  
+
 }
