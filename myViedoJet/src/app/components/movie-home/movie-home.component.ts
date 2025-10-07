7de6cb8d0814fcd29c45c3d121e2ff0addc24d39
louis.guichard-montguers <louis.guichard-montguers@etu.uca.fr>
2025-10-07 18:56:11 +0200

    filtre pas fini

--- myViedoJet/src/app/components/movie-home/movie-home.component.ts
+++ myViedoJet/src/app/components/movie-home/movie-home.component.ts
@@ -4,13 +4,15 @@ import {Movie} from '../../models/Movie';
 import {RouterLink} from '@angular/router';
 import { MovieMenuComponent } from '../movie-menu/movie-menu.component';
 import { CommonModule } from '@angular/common';
+import { CategoryService } from '../../services/category.service';
+import { Category } from '../../models/Category';
 
 type MovieWithSeen = Movie & { seen: boolean };
 
 @Component({
   selector: 'app-movie-home',
   imports: [
-    RouterLink, MovieMenuComponent, CommonModule,
+    RouterLink, MovieMenuComponent, CommonModule
   ],
   templateUrl: './movie-home.component.html',
   styleUrl: './movie-home.component.css'
@@ -20,21 +22,50 @@ type MovieWithSeen = Movie & { seen: boolean };
 
 export class MovieHomeComponent {
   protected movies: MovieWithSeen[] = []
-  constructor(private service: VideoService) { }
+  protected filteredMovies: MovieWithSeen[] = [];
+  protected filterName: string = '';
+  protected filterCategory: Category | null = null;
+  protected filterSeen: boolean | null = null; 
+  protected categories: Category[] = [];
+
+
+  constructor(private service: VideoService, private Categoryservice: CategoryService) {
+    this.Categoryservice.getCategory().subscribe( data => {
+      this.categories = data
+    })
+
+    
+   }
 
   ngOnInit() {
+
     this.service.getMovies().subscribe(data => {
     this.movies = data.map(movie => ({ ...movie, seen: false }));
     console.log(data)
+    this.filteredMovies = [...this.movies];
+    this.applyFilters();
   });
 
+  
+      
   }
+
+  applyFilters() {
+  this.filteredMovies = this.movies.filter(movie => {
+    const matchesName = movie.name.toLowerCase().includes(this.filterName.toLowerCase());
+    const matchesCategory = this.filterCategory ? movie.category.id === this.filterCategory.id : true;
+    const matchesSeen = this.filterSeen === null ? true : movie.seen === this.filterSeen;
+    return matchesName && matchesCategory && matchesSeen;
+  });
+}
+
 
   toggleSeen(movie: MovieWithSeen) {
     movie.seen = !movie.seen;
+    this.applyFilters();
     console.log(movie.name, 'vu =', movie.seen);
   }
 
-
+  
 
 }
